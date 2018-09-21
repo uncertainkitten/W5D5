f97b421037cb17fd9a23484a240a729c2951fab5
Cassandra <uncertainkitten@gmail.com>
2018-09-21 16:51:27 -0700

    End of Day commit

--- intro_to_callbacks/absurd_bubble_sort.js
+++ intro_to_callbacks/absurd_bubble_sort.js
@@ -7,7 +7,19 @@ const reader = readline.createInterface({
 
 // Write this first.
 function askIfGreaterThan(el1, el2, callback) {
-  reader.question(`Is ${el1} bigger than ${el2}?  Sorry, our sort is out of order`, function(bool))
+  reader.question(`Is ${el1} bigger than ${el2}?  Sorry, our sort is out of order \n`, function(bool){
+    if (bool === "yes") {
+      callback(true);
+    }
+    else if (bool === "no") {
+      callback(false);
+
+    }
+    else {
+      console.log("our parser is broken too \n");
+    }
+    reader.close();
+  });
   // Prompt user to tell us whether el1 > el2; pass true back to the
   // callback if true; else false.
 }
@@ -17,6 +29,15 @@ function innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop) {
   // Do an "async loop":
   // 1. If (i == arr.length - 1), call outerBubbleSortLoop, letting it
   //    know whether any swap was made.
+  if (i == arr.length - 1){
+    askIfGreaterThan(arr[i],arr[i+1],function(isGreaterThan){
+      if (isGreaterThan === true){
+        let temp = arr[i+1];
+        arr[i],arr[i+1] = arr[i+1],arr[i];
+      }
+    });
+  }
+
   // 2. Else, use `askIfGreaterThan` to compare `arr[i]` and `arr[i +
   //    1]`. Swap if necessary. Call `innerBubbleSortLoop` again to
   //    continue the inner loop. You'll want to increment i for the
@@ -39,3 +60,5 @@ absurdBubbleSort([3, 2, 1], function (arr) {
   console.log("Sorted array: " + JSON.stringify(arr));
   reader.close();
 });
+
+askIfGreaterThan(9, 2, (bool) => console.log(`it's ${bool}`));
